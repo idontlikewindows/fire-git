45e8580a51c4c51a23e1873d0619a49ac0651c21
varaprasadh <varaprasadh.a@gmail.com>
2019-01-16 21:39:03 +0530

    2019-01-16T21:39:03+05:30

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const random=require('random');
 const file_path="./data.json";
 
 const makeCommit=n=>{
-    if(n===0) return simplegit.push();
+    if(n===0) return simplegit().push();
     const x=random.int(0,54);
     const y=random.int(0,6);
     const DATE=moment().subtract(1,'y').add(1,'d')
@@ -17,7 +17,7 @@ const makeCommit=n=>{
     }
     console.log("commit at",DATE);
     jsonfile.writeFile(file_path,data,()=>{
-        simplegit.add([file_path]).commit(DATE,{'--date':DATE},
+        simplegit().add([file_path]).commit(DATE,{'--date':DATE},
                  makeCommit.bind(this,--n));
     });
 }
